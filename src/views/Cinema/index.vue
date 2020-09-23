3880498c3e61b5c492f697b1755d077def92b66b
tony-dc <491275436@qq.com>
2020-09-23 22:28:26 +0800

    add createComponent

---
+++ src/views/Cinema/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div> 
+        <Header  :title='title'  />
+        <TabBar />
+    </div>
+</template>
+<script>
+  import Header from '@/components/Header'
+  import TabBar from '@/components/TabBar'
+    export default {
+        name:'Cinema',
+        data(){
+          return {
+              title:'酷喵影院'
+          }
+        },
+         components:{
+            Header,
+            TabBar
+        }
+    }
+</script>
+<style lang="scss">
+    
+</style>
