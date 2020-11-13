7ae9b8e1b4c0896e5deb9e308e0701e7a5ab270f
tony-dc <491275436@qq.com>
2020-11-13 22:54:55 +0800

    self-page

--- src/store/user/index.js
+++ src/store/user/index.js
@@ -1,21 +1,13 @@
 const state = {
     nm: window.localStorage.getItem('username') || '',
     isAdmin: false,
-<<<<<<< HEAD
     userHeader:''
-=======
-    userHeader: ''
->>>>>>> 2e655e97f750ee07fbabc3d77191426664917ccc
 }
 const mutations = {
     USER_INFO(state, payload) {
         state.nm = payload.nm,
             state.isAdmin = payload.isAdmin
-<<<<<<< HEAD
             state.userHeader=payload.userHeader
-=======
-        state.userHeader = payload.userHeader
->>>>>>> 2e655e97f750ee07fbabc3d77191426664917ccc
     }
 }
 const actions = {
--- src/views/Mine/center.vue
+++ src/views/Mine/center.vue
@@ -1,173 +1,216 @@
 <template>
-  <div>
-     <Header title="个人中心" />
-     <div class="slefInfo">
-         <img :src="$store.state.user.userHeader" alt="用户头像" class="img-default">
-          <input type="file" name="file" value="上传头像" @change='handleToupload'> 
-         <p>
-           欢迎会员：{{$store.state.user.nm}}
-           <span v-if="$store.state.user.isAdmin">
-              用户权限：管理员 
-             <a href="/admin" target="_blank">后台管理页面</a> 
-             </span>
-           <span v-else>用户权限：普通会员 </span>
-           </p>
-     </div>
-       <h2>我的订单</h2>
+  <div id="main">
+    <Header title="个人中心" />
+    <div class="selfInfo">
+      <div class="selfImg">
+        <img :src="$store.state.user.userHeader" alt="用户头像" class="img-default" />
+         <input type="file" name="file" value="" @change="handleToupload" />
+        <p>
+         <span>欢迎:</span>
+         <span v-if="$store.state.user.isAdmin">
+          <a href="/admin" target="_blank" class="admin">管理员</a>
+        </span>
+        <span v-else>普通会员</span>：
+          <span class="userInfo">{{$store.state.user.nm}}</span>
+        </p>
+        <button @touchstart="handleToBack" class="back">退出</button>
+      </div>
+      <div class="center">
+        <h2>我的订单</h2>
         <div>
-           <div>电影</div>
-           <div>商城</div>
+          <div>
+            <i class="iconfont "></i>
+           <span>电影</span>
+            </div>
+          <div>
+            <div>
+                <i class="iconfont "></i>
+            </div>
+             <span>商城</span> 
+            </div>
         </div>
-        <div>
+      </div>
+        <div class="list">
           <ul>
             <li>在线观影</li>
             <li>优惠券</li>
             <li>折后卡</li>
           </ul>
         </div>
-       <button @touchstart='handleToBack'>退出</button>
-<<<<<<< HEAD
-       <div>
-          
-          <!-- <img :src="$store.state.user.userHeader" alt="" class="img-default"> -->
-       </div>
-=======
-       <div v-if='$store.state.user.isAdmin'>用户权限：管理员 
-       <a href="/admin" target="_blank">管理员界面</a></div>
-       <div v-else>用户权限：普通用户</div>
-       <div>
-          用户头像：<input type="file" name="file" value="上传头像" @change="handleToHeader">
-          <img :src="$store.state.user.userHeader" alt="">
-       </div>
-       
->>>>>>> 2e655e97f750ee07fbabc3d77191426664917ccc
+     
+      <p>
+      </p>
+    </div>
+    <!-- <TabBar />   -->
   </div>
 </template>
 <script>
-import {messageBox} from '@/components/JS'
-import axios from 'axios'
-import {messageBox} from '@/components/JS'
+import { messageBox } from "@/components/JS";
+import axios from "axios";
+import Header from "@/components/Header";
+import TabBar from "@/components/TabBar";
 export default {
-    name:'center',
-    //路由前置守卫
-     beforeRouteEnter (to, from, next) {
-       axios.get('/api2/users/getUser').then(res=>{
-         const result=res.data.status
-         console.log(res)
-         if(result===0){
-           next(vm=>{
-              vm.$store.commit("user/USER_INFO", {
-                      nm: res.data.data.username,
-                      isAdmin:res.data.data.isAdmin,
-                      userHeader:res.data.data.userHeader
-                    })
-           })
-          window.localStorage.setItem('username',res.data.data.username)
-         }else{
-           next('/mine/login')  
-         }
-       })
-     },
-    methods:{
-      handleToBack(){
-          this.$axios.get('/api2/users/logout').then(res=>{
-            const result=res.data.status
-            if(result===0){
-               this.$store.commit('user/USER_INFO',
-               {
-               nm:'',
-               isAdmin:false,
-               userHeader:''
-               }
-               )
-               this.$router.push('/mine/login')
-            }
-          })
-      },
-<<<<<<< HEAD
-      handleToupload(ev){
-       let This=this
-       let file= ev.target.files[0]
-       //定义一个表单传输
-       let param=new FormData();
-       param.append('file',file,file.name)
-       //配置请求头信息
-       let config={
-         headers:{
-           "Content-Type":'multipart/form-data'
-         }
-       }
-       this.$axios.post('/api2/users/updateuserHeader',param,config)
-       .then(res=>{
-           const status=res.data.status
-           console.log(res)
-           if(status===0){
-                messageBox({
-                  title:'更改头像',
-                  content:'更改成功',
-                  ok:'确定',
-                  handleOk(){
-                      This.$store.commit("user/USER_INFO", {
-                      nm: This.$store.state.user.nm,
-                      isAdmin:This.$store.state.user.isAdmin,
-                      userHeader:res.data.data.userHeader+'?'+Math.random()
-                    })
-                  }
-                })
-           }else{
-                  messageBox({
-                  title:'更改头像',
-                  content:'更改失败',
-                  ok:'确定'
-                })
-           }
-       })
-=======
-      //当input发生改变时触发
-      handleToHeader(ev){
-          const file=ev.target.files[0]
-          const This=this
-          const param=new FormData()
-          param.append('file',file)
-          const config={
-             headers:{
-               "Content-Type":'multipart/form-data'
-             }
-          }
-          this.$axios.post('/api2/users/uploaduserHeader',param,config).then((res)=>{
-            const status=res.data.status
-            if(status===0){
-               messageBox({
-                 title:'修改用户头像',
-                 content:'修改成功',
-                 ok:'确定',
-                 handleOk(){
-                     This.$store.commit("user/USER_INFO", {
-                      nm: This.$store.state.user.nm,
-                      isAdmin:This.$store.state.user.isAdmin,
-                      //请缓存方法
-                      userHeader:res.data.data.userHeader+'?'+Math.random()
-                    })
-                 }
-               })
-            }else{
-               messageBox({
-                 title:'修改用户头像',
-                 content:'修改失败',
-                 ok:'确定'
-               })
-            }
-          })
->>>>>>> 2e655e97f750ee07fbabc3d77191426664917ccc
+  name: "center",
+  components: {
+    Header,
+    TabBar
+  },
+  //路由前置守卫
+  beforeRouteEnter(to, from, next) {
+    axios.get("/api2/users/getUser").then(res => {
+      const result = res.data.status;
+      console.log(res);
+      if (result === 0) {
+        next(vm => {
+          vm.$store.commit("user/USER_INFO", {
+            nm: res.data.data.username,
+            isAdmin: res.data.data.isAdmin,
+            userHeader: res.data.data.userHeader
+          });
+        });
+        window.localStorage.setItem("username", res.data.data.username);
+      } else {
+        next("/mine/login");
       }
+    });
+  },
+  methods: {
+    handleToBack() {
+      this.$axios.get("/api2/users/logout").then(res => {
+        const result = res.data.status;
+        if (result === 0) {
+          this.$store.commit("user/USER_INFO", {
+            nm: "",
+            isAdmin: false,
+            userHeader: ""
+          });
+          this.$router.push("/mine/login");
+        }
+      });
+    },
+    handleToupload(ev) {
+      let This = this;
+      let file = ev.target.files[0];
+      //定义一个表单传输
+      let param = new FormData();
+      param.append("file", file, file.name);
+      //配置请求头信息
+      let config = {
+        headers: {
+          "Content-Type": "multipart/form-data"
+        }
+      };
+      this.$axios
+        .post("/api2/users/updateuserHeader", param, config)
+        .then(res => {
+          const status = res.data.status;
+          console.log(res);
+          if (status === 0) {
+            messageBox({
+              title: "更改头像",
+              content: "更改成功",
+              ok: "确定",
+              handleOk() {
+                This.$store.commit("user/USER_INFO", {
+                  nm: This.$store.state.user.nm,
+                  isAdmin: This.$store.state.user.isAdmin,
+                  //为了清除缓存
+                  userHeader: res.data.data.userHeader + "?" + Math.random()
+                });
+              }
+            });
+          } else {
+            messageBox({
+              title: "更改头像",
+              content: "更改失败",
+              ok: "确定"
+            });
+          }
+        });
     }
-}
+  }
+};
 </script>
 <style lang="scss" scoped>
-.img-default{
-  display:block;
-  width: 150px;
-  height: 150px;
-  border-radius:50%;
+#content #main {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 100;
+  .selfInfo {
+    //  position: relative;
+    width: 100%;
+    border-top: 1px solid rgba(255, 255, 255, 0.5);
+    .selfImg {
+      width: 100%;
+      height: 120px;
+      position: relative;
+      background-color: #e44040;
+      text-align: center;
+      .img-default,input{
+        position: absolute;
+        left: 0;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        margin: auto;
+        width: 60px;
+        height: 60px;
+        border-radius: 50%;
+      }
+      input{
+        z-index: 50;
+        opacity: 0;
+      }
+      p {
+        width: 100%;
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        height: 16px;
+        font-size: 14px;
+        color: rgb(231, 245, 245);
+        line-height: 16px;
+        text-align: center;
+        padding: 5px;
+        span{
+          padding:5px;
+          letter-spacing: 2px;
+          a{
+            text-decoration: none;
+            color:skyblue;
+            padding:3px;
+            cursor:pointer;
+          }
+        }
+        .userInfo {
+          font-size: 18px;
+          font-weight: bold;
+          color: skyblue;
+        }
+      }
+      .back {
+        position: absolute;
+        bottom: 0;
+        right: 0px;
+        letter-spacing: 2px;
+        border: none;
+        border-radius: 5px;
+        padding: 3px;
+        color: slateblue;
+      }
+    }
+  }
+  .center{
+    // width:300px;
+    h2{
+      width:100%;
+      height:60px;
+      line-height: 60px;
+      text-align: center;
+      font-size:18px;
+    }
+  }
 }
 </style>
